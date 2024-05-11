c8f3d53281fca4cd1f66dcdac0278b86bd212f14
Arun Kumar <info@aryadevops.com>
2024-05-11 19:57:32 +0530

    Variables commit

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Raju
+PERSON2=Ramu
+
+cat "$ERSON1 : hi $ERSON2 how are you"
+cat "$ERSON2 : hi $ERSON1 Im good ,what about u"
+cat "$ERSON1 : Im good $ERSON2,How u doing"
+cat "$ERSON2 : Nothing $ERSON1"
