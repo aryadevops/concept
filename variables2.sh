459383cbdc3b3389bc70b20febe7ff66202fa2a5
Arun Kumar <info@aryadevops.com>
2024-05-12 10:26:29 +0530

    Add everything

---
+++ variables2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+DATE=$(date)
+
+cat "$ERSON1 : hi $ERSON2 how are you"
+cat "$ERSON2 : hi $ERSON1 Im good ,what about u"
+cat "$ERSON1 : Im good $ERSON2,How u doing"
+cat "$ERSON2 : Nothing $ERSON1"
+cat " Today date is : $DATE"
